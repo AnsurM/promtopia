b5f18788b5a4fb1bdff8af87a25535e8a8b4f301
Syed Ansur Mehdi <ansurmehdi@gmail.com>
2023-05-27 03:40:14 +0200

    finalize app

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,28 +1,51 @@
-import "@styles/globals.css";
+"use client";
 
-import Nav from "@components/Nav";
-import Provider from "@components/Provider";
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
-export const metadata = {
-  title: "Promptopia",
-  description: "Discover & Share AI Prompts",
+import Form from "@components/Form";
+
+const CreatePrompt = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
+
+  const [submitting, setIsSubmitting] = useState(false);
+  const [post, setPost] = useState({ prompt: "", tag: "" });
+
+  const createPrompt = async (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+
+    try {
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
+
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  return (
+    <Form
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
+    />
+  );
 };
 
-const RootLayout = ({ children }) => (
-  <html lang="en">
-    <body>
-      <Provider>
-        <div className="main">
-          <div className="gradient" />
-        </div>
-
-        <main className="app">
-          <Nav />
-          {children}
-        </main>
-      </Provider>
-    </body>
-  </html>
-);
-
-export default RootLayout;
+export default CreatePrompt;
