89c22136ec38aae9ebd43f58a30bc981d8a5cbb8
Syed Ansur Mehdi <ansurmehdi@gmail.com>
2023-05-27 02:27:01 +0200

    fix space

--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,7 @@ export default function Home() {
       <h1 className="head_text text-center">
         Discover & Share
         <br className="max-md:hidden" />
-        <span className="orange_gradient text-center">AI-powered Prompts</span>
+        <span className="orange_gradient text-center"> AI-powered Prompts</span>
       </h1>
       <p className="desc text-center">
         Promptopia is an open-source AI prompting tool for modern world to
